ba625ba84c063d855f5663a229fc15d676242bf9
ttala <tyeri.tala@gmail.com>
2018-10-31 15:53:37 +0100

    modify template to commit change from database

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-
+from .models import Post
+from django.contrib.auth.models import User
+from django.utils import timezone
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(author=User.objects.get(username='ttala')).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts':posts})
